bda17e942ebb890b51469eaad4a9b1182250a2e7
Thanaboon Sapmontree <thanaboon.sap@gmail.com>
2022-11-20 15:23:02 +0700

    MapParserTest-fixed

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -3,6 +3,7 @@ package nl.tudelft.jpacman.level;
 import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -11,10 +12,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This is a test class for MapParser.
@@ -29,14 +27,13 @@ public class MapParserTest {
     @Mock
     private Blinky blinky;
 
-    private final int boardCreatedFail = 10;
 
     /**
      * Test for the parseMap method (good map).
      */
     @Test
     public void testParseMapGood() {
-        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.initMocks(this);
         assertNotNull(boardFactory);
         assertNotNull(levelFactory);
         Mockito.when(levelFactory.createGhost()).thenReturn(blinky);
@@ -47,7 +44,7 @@ public class MapParserTest {
         map.add("############");
         mapParser.parseMap(map);
         Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
-        Mockito.verify(boardFactory, Mockito.times(boardCreatedFail)).createGround();
+        Mockito.verify(boardFactory, Mockito.times(10)).createGround();
     }
 
     /**
@@ -57,7 +54,7 @@ public class MapParserTest {
     public void testParseMapWrong() {
         PacmanConfigurationException thrown =
             assertThrows(PacmanConfigurationException.class, () -> {
-                MockitoAnnotations.openMocks(this);
+                MockitoAnnotations.initMocks(this);
                 assertNotNull(boardFactory);
                 assertNotNull(levelFactory);
                 MapParser mapParser = new MapParser(levelFactory, boardFactory);
